add718ee8eb5155574271f7237dbaf77df703eaf
yuriBaza23 <yuribaza2321@gmail.com>
2021-02-24 02:32:03 -0300

    create users and handle response whit render

---
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -0,0 +1,15 @@
+defmodule RocketpayWeb.UsersController do
+  use RocketpayWeb, :controller
+
+  def create(conn, params) do
+    params
+    |> Rocketpay.create_user()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %Rocketpay.User{} = user}, conn) do
+    conn
+    |> put_status(:created)
+    |> render("create.json", user: user)
+  end
+end
