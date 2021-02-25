f1e6a392b83154e4ea3563b3d229a99c89f570c1
yuriBaza23 <yuribaza2321@gmail.com>
2021-02-25 20:42:06 -0300

    add accounts routes

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -8,20 +8,14 @@ defmodule RocketpayWeb.Router do
   scope "/api", RocketpayWeb do
     pipe_through :api
 
-    get "/challenge/:str", ChallengeController, :index
-
     get "/:filename", WelcomeController, :index
 
     post "/users", UsersController, :create
+
+    post "/accounts/:id/deposit", AccountsController, :deposit
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
