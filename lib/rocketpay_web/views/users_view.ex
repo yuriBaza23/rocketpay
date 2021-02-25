645a39a7989b8ce3aeeddd6e043fea7a3f1b45db
yuriBaza23 <yuribaza2321@gmail.com>
2021-02-25 20:41:29 -0300

    users view

--- lib/rocketpay_web/views/users_view.ex
+++ lib/rocketpay_web/views/users_view.ex
@@ -1,11 +1,24 @@
 defmodule RocketpayWeb.UsersView do
-  def render("create.json", %{user: %Rocketpay.User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+    user: %Rocketpay.User{
+      id: id,
+      name: name,
+      nickname: nickname,
+      account: %Rocketpay.Account{
+        id: account_id,
+        balance: balance
+      }
+    }}) do
     %{
       message: "User Created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
