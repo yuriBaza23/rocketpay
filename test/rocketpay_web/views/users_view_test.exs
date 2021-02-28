893fbb1d94b1289788cf578242623e301e90c8e5
yuriBaza23 <yuribaza2321@gmail.com>
2021-02-28 13:42:45 -0300

    Users View Test

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,36 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Yuri",
+      password: "olamundo",
+      nickname: "bazinha",
+      email: "yuribaza@gmail.com",
+      age: 20
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} = Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response =
+      %{message: "User Created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Yuri",
+        nickname: "bazinha"
+    }}
+
+    assert expected_response == response
+  end
+end
